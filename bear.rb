8d49f11d94c7a2cfeea43ab121fc53e58d718642
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-23 16:55:03 +0100

    created first fish and passed specs test

--- bear.rb
+++ bear.rb
@@ -5,6 +5,7 @@ attr_reader :name
 def initialize(name, type)
   @name = name
   @type = type
+  @stomach = []
 end
 
 
---
+++ specs/fish_spec.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../fish')
+
+
+class FishTest < MiniTest::Test
+
+  def setup
+    @fish1 = Fish.new("Steve the Fish")
+  end
+
+
+  def test_fish_has_name
+    expected = "Steve the Fish"
+    actual = @fish1.name
+    assert_equal(expected, actual)
+  end
+
+
+end
